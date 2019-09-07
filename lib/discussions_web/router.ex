9c282df6c8979495bd02f5c61ef9d2c20c630521
linqueta <lincolnrodrs@gmail.com>
2019-09-07 11:27:48 -0300

    add topics routes

--- lib/discussions_web/router.ex
+++ lib/discussions_web/router.ex
@@ -18,6 +18,7 @@ defmodule DiscussionsWeb.Router do
 
     get "/", PageController, :index
     resources "/users", UserController
+    resources "/topics", TopicController
   end
 
   # Other scopes may use custom stacks.
