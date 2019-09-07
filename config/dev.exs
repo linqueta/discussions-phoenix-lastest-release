0fdc37fa02d6df0b1f7dfe9ea3f2e4cfc409a475
linqueta <lincolnrodrs@gmail.com>
2019-09-07 11:10:51 -0300

    change local database infos

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :discussions, Discussions.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "linqueta",
+  password: "linqueta",
   database: "discussions_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
